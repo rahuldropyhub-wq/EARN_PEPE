cf2a29cc7c6c4a375d0e1518e41294d348dddb76
balajiprojects049 <balajiprojects049@gmail.com>
2026-08-15 19:33:28 +0530

    Added vercel.json to prevent 404 errors

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 const AUTH_KEY = 'earnpepe_admin_auth';
 
 export const loginAdmin = async (username, password) => {
